c2521ace7a2c86299aca0c27f915b0773a489612
unknown <lethanhbinhkts@gmail.com>
2023-01-11 12:39:43 +0700

    parse address

--- src/main/java/cnpm/quanlynhankhau/models/DiaChi.java
+++ src/main/java/cnpm/quanlynhankhau/models/DiaChi.java
@@ -22,7 +22,21 @@ public class DiaChi {
      */
     public static DiaChi parse(CharSequence text) {
         // TODO parse dia chi theo format toString dưới
-        return null;
+     	String thanhPho1 = null, quan1 = null, duongPho1 = null, soNha1 = null, ghiChu1 = null;
+     	String output = String.valueOf(text);
+     	output = output.replaceAll(" Số ", "");
+     	output = output.replaceAll(" Đường ", "");
+     	output = output.replaceAll(" Quận ", "");
+     	output = output.replaceAll(" Thành Phố ", "");
+ 
+     	String[] splited = output.split("[,]", 0);
+     	ghiChu1 = splited[0];
+     	soNha1 = splited[1];
+     	duongPho1 = splited[2];
+     	quan1 = splited[3];
+     	thanhPho1 = splited[4];
+		DiaChi x = new DiaChi(thanhPho1, quan1, duongPho1, soNha1, ghiChu1);
+    	return x;
     }
 
 
@@ -44,5 +58,13 @@ public class DiaChi {
         if (thanhPho != null) x.append("Thành phố ").append(thanhPho).append(".");
 
         return x.toString();
+    }
+    public static void main(String[] args) {
+    	String thanhPho1 = null, quan1 = null, duongPho1 = null, soNha1 = null, ghiChu1 = null;
+    	String test = new String();
+    	test = "Đại Học Bách Khoa, Số 1, Đường Giải Phóng, Quận Hai Bà Trưng, Thành Phố Hà Nội";
+    	DiaChi test1 = new DiaChi(thanhPho1, quan1, duongPho1, soNha1, ghiChu1);
+    	test1 = parse(test);
+    	System.out.print(test1.ghiChu +" "+ test1.soNha +" "+ test1.duongPho +" "+ test1.quan +" "+ test1.thanhPho);
     }
 }
