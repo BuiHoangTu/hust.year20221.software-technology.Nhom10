faf6ceb949751d704b96620d505c6e688459f111
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-01-26 22:51:10 +0700

    initialize TTHS

--- TraoThuongHS/src/main/java/module-info.java
+++ TraoThuongHS/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module cnpm.traothuonghs {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires cnpm.quanlynhankhau;
 
 
-	opens cnpm.traothuonghs to javafx.fxml;
-	exports cnpm.traothuonghs;
+	opens cnpm.traothuonghs.models to javafx.fxml;
+	exports cnpm.traothuonghs.models;
 }
