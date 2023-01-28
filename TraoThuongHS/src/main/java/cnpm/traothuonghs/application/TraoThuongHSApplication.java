b2dea9fef221de5c7cc81edd4aec2451151591e4
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-01-28 13:11:17 +0700

    move danhHieu to TinhThuong

--- TraoThuongHS/src/main/java/cnpm/traothuonghs/application/TraoThuongHSApplication.java
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/application/TraoThuongHSApplication.java
@@ -9,30 +9,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class TraoThuongHSApplication extends Application {
-    public static final DiaChi CO_SO_HIEN_TAI = new DiaChi("Hà Nội", "Hai Bà Trưng", "Đại Cồ Việt", "1", "Trụ Sở Quản Lý Nhân Khẩu Bách Khoa");
-    public static int USER;
-    public static Stage MAIN_STAGE;
+	public static final DiaChi CO_SO_HIEN_TAI = new DiaChi("Hà Nội", "Hai Bà Trưng", "Đại Cồ Việt", "1", "Trụ Sở Quản Lý Nhân Khẩu Bách Khoa");
+	public static int USER;
+	public static Stage MAIN_STAGE;
 
-    @Override
-    public void start(Stage stage){
-        FXMLLoader fxmlLoader = new FXMLLoader(TraoThuongHSApplication.class.getResource("/cnpm/quanlynhankhau/views/Login-view.fxml"));
-        Scene scene = null;
-        try {
-            scene = new Scene(fxmlLoader.load());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Image icon = new Image(" "); /*path to icon */
+	@Override
+	public void start(Stage stage){
+		FXMLLoader fxmlLoader = new FXMLLoader(TraoThuongHSApplication.class.getResource("/cnpm/quanlynhankhau/views/Login-view.fxml"));
+		Scene scene = null;
+		try {
+			scene = new Scene(fxmlLoader.load());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//Image icon = new Image(" "); /*path to icon */
 
-        stage.setTitle("Quản Lý Nhân Khẩu");
-        //stage.getIcons().add(icon);
-        stage.setScene(scene);
-        stage.show();
+		stage.setTitle("Quản Lý Nhân Khẩu");
+		//stage.getIcons().add(icon);
+		stage.setScene(scene);
+		stage.show();
 
-        MAIN_STAGE = stage;
-    }
+		MAIN_STAGE = stage;
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 }
