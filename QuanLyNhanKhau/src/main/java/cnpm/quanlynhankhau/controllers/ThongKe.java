4c4eac4ddee721bd8c366c6ad8f50489c8db9096
Henry-n-Nguyen <70119608+Henry-n-Nguyen@users.noreply.github.com>
2023-01-29 20:44:53 +0700

    Thêm khung Controller 8,9,10 Giao diện bài 1

---
+++ QuanLyNhanKhau/src/main/java/cnpm/quanlynhankhau/controllers/ThongKe.java
@@ -0,0 +1,41 @@
+package cnpm.quanlynhankhau.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ThongKe {
+	@FXML
+    private ComboBox cbGioiTinh;
+	@FXML
+    private ComboBox cbTinhTrang;
+	@FXML
+    private TextField tfAgeFrom;
+	@FXML
+    private TextField tfAgeUntil;
+	@FXML
+    private TextField tfYearFrom;
+	@FXML
+    private TextField tfYearUntil;
+	@FXML
+    private TableView tvThanhVien;
+
+	@FXML
+    protected void onShowClicked() {
+        
+    }
+	
+	@FXML
+    protected void onExportClicked() {
+        
+    }
+	
+    @FXML
+    protected void onHuyClicked() {
+        
+    }
+    
+    @FXML
+    protected void onXacNhanClicked() {
+        
+    }
+}
