c52afba2bab8f8462570d511a998e57222afed04
Nguyễn Hoàng Hiệp <113684719+Hiepnh1211@users.noreply.github.com>
2023-01-31 20:49:47 +0700

    TODO 30/1

--- QuanLyNhanKhau/src/main/java/cnpm/quanlynhankhau/controllers/TrangChinhControllers.java
+++ QuanLyNhanKhau/src/main/java/cnpm/quanlynhankhau/controllers/TrangChinhControllers.java
@@ -1,12 +1,31 @@
 package cnpm.quanlynhankhau.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import cnpm.quanlynhankhau.models.Database;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class TrangChinhControllers extends EdgeController{
     public Pane pnlTrangChu;
+    public Label lblNhanKhauNum;
+    public Label lblHoKhauNum;
+    public Label lblTamTruNum;
+    public Label lblTamVangNum;
+
+    private void initialize() throws SQLException {
+//        PreparedStatement nhanKhauNum = Database.getConnection().prepareStatement("Select COUNT(maNhanKhau) from quan_ly_nhan_khau.nhan_khau");
+//        ResultSet num = nhanKhauNum.executeQuery();
+//        num.next();
+//        lblNhanKhauNum.setText(num.getString(1));
+//
+//        PreparedStatement hoKhauNum = Database.getConnection().prepareStatement("Select COUNT(maHoKhau) from quan_ly_nhan_khau.ho_khau");
+//        num = hoKhauNum.executeQuery();
+//        num.next();
+//        lblHoKhauNum.setText(num.getString(1));
+    }
+
 
 }
