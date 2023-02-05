783eaaf41726866a0cd3ca0f7d789f9cb812ca4f
unknown <lethanhbinhkts@gmail.com>
2023-02-05 12:55:03 +0700

    thêm hàm change cho HoKhau và NhanKhau, populate data cho trao thưởng học sinh

--- QuanLyNhanKhau/src/test/java/cnpm/quanlynhankhau/models/HoKhauTest.java
+++ QuanLyNhanKhau/src/test/java/cnpm/quanlynhankhau/models/HoKhauTest.java
@@ -1,6 +1,10 @@
 package cnpm.quanlynhankhau.models;
 
+import cnpm.quanlynhankhau.services.HoKhauService;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +14,11 @@ class HoKhauTest {
 
     void commit() {
 
+    }
+
+    @Test
+    void change() throws SQLException {
+        HoKhau HKtest = HoKhauService.getHoKhau("3");
+        HKtest.change(null, "HN05",null, LocalDate.now());
     }
 }
