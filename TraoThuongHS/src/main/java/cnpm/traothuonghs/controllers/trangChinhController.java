01805412dc6891aa2282a7212ee4683978771913
Nguyễn Hoàng Hiệp <113684719+Hiepnh1211@users.noreply.github.com>
2023-02-08 16:10:31 +0700

    trangChinh+Controller

---
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/trangChinhController.java
@@ -0,0 +1,30 @@
+package cnpm.traothuonghs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class trangChinhController {
+    public Label lblHocSinhNum;
+    public Label lblPhanThuongTraoNum;
+    public Label lblPhanThuongConNum;
+
+    public void onHocSinhClicked(ActionEvent event) {
+    }
+
+    public void onTiLePhanTHuongClicked(ActionEvent event) {
+    }
+
+    public void onTrangChuClicked(ActionEvent event) {
+    }
+
+    public void onThongKeClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void initialize() {
+        //Lấy thông tin số học sinh                 |
+        //lấy thông tin số phần thưởng được trao    | từ database
+        //lấy thông tin số phần thưởng còn lại      |
+    }
+}
