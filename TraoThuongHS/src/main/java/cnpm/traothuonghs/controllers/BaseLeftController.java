563d09211985706e01d2b2a51dbf00d69152209a
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-02-08 10:49:32 +0700

    add base view-controller

---
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/BaseLeftController.java
@@ -0,0 +1,26 @@
+package cnpm.traothuonghs.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public abstract class BaseLeftController extends ChangeSceneControllers{
+	@FXML
+	public void onHomeClicked(ActionEvent ignored) {
+		changeScene(""); // link to home
+	}
+
+	@FXML
+	public void onHSClicked(ActionEvent ignored) {
+		// link to hs
+	}
+
+	@FXML
+	public void onPTClicked(ActionEvent ignored) {
+		// link ti pt
+	}
+
+	@FXML
+	public void onTKClicked(ActionEvent ignored) {
+		// link to tk
+	}
+}
