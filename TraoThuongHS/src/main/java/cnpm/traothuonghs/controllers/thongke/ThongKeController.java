388ae08e062352c7134577d3e1d982af01141f71
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-02-09 14:16:54 +0700

    add previous to controller

--- TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/thongke/ThongKeController.java
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/thongke/ThongKeController.java
@@ -1,8 +1,10 @@
 package cnpm.traothuonghs.controllers.thongke;
 
+import cnpm.traothuonghs.application.TraoThuongHSApplication;
 import cnpm.traothuonghs.controllers.BaseLeftController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -20,14 +22,19 @@ public class ThongKeController extends BaseLeftController {
 	@FXML
 	protected ToggleGroup filterType;
 	// endregion
+	private Scene previousScene;
 
 
-	public ThongKeController() {}
+	public ThongKeController(Scene previousScene) {
+		this.previousScene = previousScene;
+	}
 	/**
 	 * Tạo controller với chọn sẵn 1 loại lọc
 	 * @param selectedToggle loại lọc. BY_DOT : lọc theo đợt phát, BY_HO : lọc theo hộ
 	 */
-	public ThongKeController(RadioButton selectedToggle) {
+	public ThongKeController(Scene previousScene, RadioButton selectedToggle) {
+		this.previousScene = previousScene;
+
 		if (BY_DOT.equals(selectedToggle)) {
 			filterType.selectToggle(rbDot);
 		} else if (BY_HO.equals(selectedToggle)) {
@@ -51,5 +58,6 @@ public class ThongKeController extends BaseLeftController {
 
 	@FXML
 	protected void onReturnClicked(ActionEvent ignored) {
+		TraoThuongHSApplication.MAIN_STAGE.setScene(previousScene);
 	}
 }
--- TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/thongke/ThongKeScreenController.java
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/thongke/ThongKeScreenController.java
@@ -1,5 +1,6 @@
 package cnpm.traothuonghs.controllers.thongke;
 
+import cnpm.traothuonghs.application.TraoThuongHSApplication;
 import cnpm.traothuonghs.controllers.BaseLeftController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,15 +8,17 @@ import javafx.fxml.FXML;
 public class ThongKeScreenController extends BaseLeftController{
     @FXML
     protected void onGDClicked(ActionEvent ignoredEvent) {
-        changeScene("/cnpm/traothuonghs/views/thongke/ThongKeScreen-view.fxml", new ThongKeController()); // todo add link, controller
+        changeScene("/cnpm/traothuonghs/views/thongke/ThongKeScreen-view.fxml", new ThongKeController(TraoThuongHSApplication.MAIN_STAGE.getScene()));
     }
 
 	@FXML
 	protected void onDotClicked(ActionEvent ignoredEvent) {
+		changeScene("/cnpm/traothuonghs/views/thongke/ThongKeScreen-view.fxml", new ThongKeController(TraoThuongHSApplication.MAIN_STAGE.getScene(), ThongKeController.BY_DOT));
 	}
 
 	@FXML
 	protected void onRawClicked(ActionEvent ignoredEvent) {
+		changeScene("/cnpm/traothuonghs/views/thongke/ThongKeScreen-view.fxml", new ThongKeController(TraoThuongHSApplication.MAIN_STAGE.getScene(), ThongKeController.BY_HO));
 	}
 
 }
