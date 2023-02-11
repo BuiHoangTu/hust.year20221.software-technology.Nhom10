ce3fbad1120fd252ee37a12a2ce90c5c07fdaef6
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-02-12 00:14:33 +0700

    add login

---
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/LoginController.java
@@ -0,0 +1,49 @@
+package cnpm.traothuonghs.controllers;
+
+import cnpm.quanlynhankhau.application.QuanLyNhanKhauApplication;
+import cnpm.quanlynhankhau.services.Database;
+import cnpm.traothuonghs.application.TraoThuongHSApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class LoginController {
+    @FXML
+    private TextField tfUserName;
+    @FXML
+    private TextField tfPasswd;
+    @FXML
+    private Label lWarning;
+
+    public LoginController() {
+    }
+
+    @FXML
+    protected void onClickConfirmLogin() {
+        try {
+            int uID = Database.login(tfUserName.getText(), tfPasswd.getText());
+            if (uID >= 0) {
+				QuanLyNhanKhauApplication.USER = uID;
+                FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("/cnpm/traothuonghs/views/Trang_chinh.fxml"));
+                Scene scene = null;
+                try {
+                    scene = new Scene(fxmlLoader.load());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                TraoThuongHSApplication.MAIN_STAGE.setScene(scene);
+            } else {
+                lWarning.setVisible(true);
+                lWarning.setText("Your account or password is wrong!");
+            }
+        } catch (SQLException e) {
+            lWarning.setVisible(true);
+            lWarning.setText("Server Error! Please wait a minute and re-try");
+        }
+    }
+}
