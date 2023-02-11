98afcfdadcdb14992b1f0c2732dac6af79962385
Nguyễn Hoàng Hiệp <113684719+Hiepnh1211@users.noreply.github.com>
2023-02-11 22:41:59 +0700

    trangChinh view

--- TraoThuongHS/src/main/java/module-info.java
+++ TraoThuongHS/src/main/java/module-info.java
@@ -7,5 +7,10 @@ module cnpm.traothuonghs {
 
 	exports cnpm.traothuonghs.models;
 	opens cnpm.traothuonghs.models to javafx.fxml;
-
+	exports cnpm.traothuonghs.controllers;
+	opens cnpm.traothuonghs.controllers to javafx.fxml;
+	exports cnpm.traothuonghs.application;
+	opens cnpm.traothuonghs.application to javafx.fxml;
+	exports cnpm.traothuonghs.services;
+	opens cnpm.traothuonghs.services to javafx.fxml;
 }
