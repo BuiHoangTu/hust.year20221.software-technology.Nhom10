1bdf0f3ea73a0ac720abd79e20e629482c996954
Nguyễn Hoàng Hiệp <113684719+Hiepnh1211@users.noreply.github.com>
2023-02-16 23:25:57 +0700

    TODO 15/2 and Done ?

--- TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/hocsinh/ThemHocSinhController.java
+++ TraoThuongHS/src/main/java/cnpm/traothuonghs/controllers/hocsinh/ThemHocSinhController.java
@@ -39,6 +39,12 @@ public class ThemHocSinhController extends ChangeSceneControllers implements IFl
 
     public void onXacNhanClicked(ActionEvent event) throws SQLException {
         //Thêm học sinh vào database
+        if(tfTenHocSinh.getText().equals("")|| tfLop.getText().equals("") || tfTenPhuHuynh.getText().equals("") || tfDanhHieu.getText().equals("") || tfMaHoKhau.getText().equals("") || tfDotPhatThuong.getText().equals("") || cbTruong.getValue().toString().equals("") || dpNgaySinh.getValue().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Thông tin không đầy đử, mời bổ sung thêm");
+            alert.show();
+            return;
+        }
         HocSinhService.themHocSinh(tfTenHocSinh.getText(), tfTenPhuHuynh.getText(), dpNgaySinh.getValue(), cbTruong.getValue().toString(), tfLop.getText(), tfMaHoKhau.getText(), tfDanhHieu.getText(),tfDotPhatThuong.getText());
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText("Học sinh: " + tfTenHocSinh.getText() + "\nSinh ngày: " + dpNgaySinh.getValue() + "\nHọc trường: " + cbTruong.getValue().toString() + "\nLớp: " + tfLop.getText() + "\nĐã được thêm thành công");
